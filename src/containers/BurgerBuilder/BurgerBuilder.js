f082c0f2b7a311f523661babcc03b0ac6e35dae9
Jefersson Bustamante <jeferb93@gmail.com>
2019-06-20 16:34:02 -0500

    inicializacion de rutas

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -31,6 +31,7 @@ class BurgerBuilder extends Component{
     }
 
     componentDidMount () {
+        console.log(this.props)
         axios.get('https://test-react-js-jb.firebaseio.com/ingrdients.json')
             .then(response => {
                 this.setState({ingredients:response.data})
@@ -94,28 +95,28 @@ class BurgerBuilder extends Component{
     }
 
     purchaseContinueHandler = () => {
-        this.setState({loading:true})
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Jefersson Bustamante',
-                address:{
-                    street: 'teststreet 1',
-                    zipCode: '41531',
-                    country: 'Colombia'
-                },
-                email: 'test@gmail.com'
-            },
-            deliveryMethod: 'fastest'
-        }
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({loading:false, purchasing: false});
-            })
-            .catch(error => {
-                this.setState({loading:false, purchasing: false});
-            });
+        // this.setState({loading:true})
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Jefersson Bustamante',
+        //         address:{
+        //             street: 'teststreet 1',
+        //             zipCode: '41531',
+        //             country: 'Colombia'
+        //         },
+        //         email: 'test@gmail.com'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // }
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         this.setState({loading:false, purchasing: false});
+        //     })
+        //     .catch(error => {
+        //         this.setState({loading:false, purchasing: false});
+        //     });
     }
 
     render(){
